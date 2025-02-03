5e414d7e7aff6d2dd3e10cca145d7fe32b16851b
kya <dbsdk4211@naver.com>
2025-02-03 22:02:43 +0900

    home2

---
+++ src/Homework/VideoShop/Video.java
@@ -0,0 +1,27 @@
+package Homework.VideoShop;
+
+public class Video extends Content {
+    String genre;
+
+    public Video(String title, String genre) {
+        super(title);
+        this.genre = genre;
+        totalPrice();
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    void totalPrice() {
+        if (genre.equals("new")) setPrice(2000);
+        else if (genre.equals("comic")) setPrice(1500);
+        else if (genre.equals("child")) setPrice(1000);
+        else setPrice(500);
+    }
+}
