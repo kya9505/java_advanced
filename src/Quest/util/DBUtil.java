aa7aa353e0434c763016c490c115892c0e2615aa
kya <dbsdk4211@naver.com>
2025-02-28 13:51:38 +0900

    board

---
+++ src/Quest/util/DBUtil.java
@@ -0,0 +1,45 @@
+package Quest.util;
+
+import lombok.Data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+/**
+ * packageName   : jdbc.advanced.users
+ * fileName      : DBUtil
+ * author        : a
+ * date          : 2025-02-28
+ * description   : 데이터베이스 연결 작업 클래스
+ * =================================================
+ * DATE             AUTHOR             NOTE
+ * -------------------------------------------------
+ * 2025-02-28        a
+ */
+
+@Data
+public class DBUtil {
+//    private static ResourceBundle bundle;
+   static { // 클래스명으로 호출하려고?
+//       bundle = ResourceBundle.getBundle("Quest.cofing.dbinfo");
+       try {
+           Class.forName("com.mysql.cj.jdbc.Driver"); // db 드라이버 조회
+       } catch (
+               ClassNotFoundException e) {
+           throw new RuntimeException(e);
+       }
+   }
+
+   public static Connection getConnection(){
+       Connection conn = null;
+       try {
+           conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/questdb?serverTimezone=Asia/Seoul" ,"root","root");
+           return conn;
+       } catch (SQLException e) {
+           System.out.println("연결실패");
+           return null;
+       }
+   }
+}
