9945a6594d14707fd5c0d13fe1a91f9e742643e4
kya <dbsdk4211@naver.com>
2025-03-05 00:24:20 +0900

    fix : search bug

--- src/Quest/dao/DBUtil.java
+++ src/Quest/dao/DBUtil.java
@@ -7,24 +7,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-/**
- * packageName   : jdbc.advanced.users
- * fileName      : DBUtil
- * author        : a
- * date          : 2025-02-28
- * description   : 데이터베이스 연결 작업 클래스
- * =================================================
- * DATE             AUTHOR             NOTE
- * -------------------------------------------------
- * 2025-02-28        a
- */
-
 @Data
 public class DBUtil {
 
     private static ResourceBundle bundle;
     static {
-        bundle = ResourceBundle.getBundle("HRmanager0304.cofing.dbinfo");
+        bundle = ResourceBundle.getBundle("HRmanager0304.config.dbinfo");
         try {
             Class.forName(bundle.getString("driver")); // db 드라이버 조회
         } catch (
